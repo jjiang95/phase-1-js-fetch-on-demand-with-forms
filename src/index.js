9e03ae4f16d8c4bed638296c24227c1f651b0a6b
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-04-16 17:53:55 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = e.target.children[1].value;
+
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then((resp) => resp.json())
+    .then((json) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = json.title;
+        summary.innerText = json.summary;
+    });
+  });
+};
 
 document.addEventListener('DOMContentLoaded', init);
